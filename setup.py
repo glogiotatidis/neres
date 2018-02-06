65d4c88b528e6aed3b977afc1dac76163efaa477
Giorgos Logiotatidis <seadog@sealabs.net>
2018-02-06 14:37:15 +0200

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='neres',
-    version='0.4.2',
+    version='0.4.3',
     description="(unofficial) NewRelic Synthetics CLI",
     long_description=readme + '\n\n' + history,
     author="Giorgos Logiotatidis",
